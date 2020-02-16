43c18066fa962d69b78d80aabab747e5c85a40ae
Joel Clifford <joel.clifford.bootcamp@gmail.com>
2020-02-16 11:52:27 -0500

    added scripts and other references

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+var homeworkLinks = {};
+homeworkLinks["HTML, Git & CSS"] = "https://joel-clifford-bootcamp.github.io/homework-1/";
+
+
+const homeWorkStatus = 
+{
+    NotStarted = 0,
+    InProgress = 1,
+}
+
+class Homework
+{
+    constructor(title,link){
+        this.title = title;
+        this.link = link;
+    }
+}
+
+
+
+
+
